dcc111533056999ad148736cd20da9c9de640f8b
Thomas Yuanke <60814001+ThomasXIONG151215@users.noreply.github.com>
2022-08-03 20:15:45 +0800

    Add files via upload

---
+++ plotly_dirgraph_master/addEdge.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 11:45:07 2020
+
+@author: aransil
+"""
+
+import math
+from typing import List
+from itertools import chain
+
+# Start and end are lists defining start and end points
+# Edge x and y are lists used to construct the graph
+# arrowAngle and arrowLength define properties of the arrowhead
+# arrowPos is None, 'middle' or 'end' based on where on the edge you want the arrow to appear
+# arrowLength is the length of the arrowhead
+# arrowAngle is the angle in degrees that the arrowhead makes with the edge
+# dotSize is the plotly scatter dot size you are using (used to even out line spacing when you have a mix of edge lengths)
+def addEdge(start, end, edge_x, edge_y, lengthFrac=1, arrowPos = None, arrowLength=0.025, arrowAngle = 30, dotSize=20):
+
+    # Get start and end cartesian coordinates
+    x0, y0 = start
+    x1, y1 = end
+
+    # Incorporate the fraction of this segment covered by a dot into total reduction
+    length = math.sqrt( (x1-x0)**2 + (y1-y0)**2 )
+    dotSizeConversion = .0565/20 # length units per dot size
+    convertedDotDiameter = dotSize * dotSizeConversion
+    lengthFracReduction = convertedDotDiameter / length
+    lengthFrac = lengthFrac - lengthFracReduction
+
+    # If the line segment should not cover the entire distance, get actual start and end coords
+    skipX = (x1-x0)*(1-lengthFrac)
+    skipY = (y1-y0)*(1-lengthFrac)
+    x0 = x0 + skipX/2
+    x1 = x1 - skipX/2
+    y0 = y0 + skipY/2
+    y1 = y1 - skipY/2
+
+    # Append line corresponding to the edge
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None) # Prevents a line being drawn from end of this edge to start of next edge
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+
+    # Draw arrow
+    if not arrowPos == None:
+
+        # Find the point of the arrow; assume is at end unless told middle
+        pointx = x1
+        pointy = y1
+
+        eta = math.degrees(math.atan((x1-x0)/(y1-y0))) if y1!=y0 else 90.0
+
+        if arrowPos == 'middle' or arrowPos == 'mid':
+            pointx = x0 + (x1-x0)/2
+            pointy = y0 + (y1-y0)/2
+
+        # Find the directions the arrows are pointing
+        signx = (x1-x0)/abs(x1-x0) if x1!=x0 else +1    #verify this once
+        signy = (y1-y0)/abs(y1-y0) if y1!=y0 else +1    #verified
+
+        # Append first arrowhead
+        dx = arrowLength * math.sin(math.radians(eta + arrowAngle))
+        dy = arrowLength * math.cos(math.radians(eta + arrowAngle))
+        edge_x.append(pointx)
+        edge_x.append(pointx - signx**2 * signy * dx)
+        edge_x.append(None)
+        edge_y.append(pointy)
+        edge_y.append(pointy - signx**2 * signy * dy)
+        edge_y.append(None)
+
+        # And second arrowhead
+        dx = arrowLength * math.sin(math.radians(eta - arrowAngle))
+        dy = arrowLength * math.cos(math.radians(eta - arrowAngle))
+        edge_x.append(pointx)
+        edge_x.append(pointx - signx**2 * signy * dx)
+        edge_x.append(None)
+        edge_y.append(pointy)
+        edge_y.append(pointy - signx**2 * signy * dy)
+        edge_y.append(None)
+
+
+    return edge_x, edge_y
+
+def addEdge_3D(start, end, edge_x, edge_y, edge_z, lengthFrac=1, arrowPos = None, arrowLength=0.025, arrowAngle = 30, dotSize=20):
+    
+    # Get start and end cartesian coordinates
+    x0, y0, z0 = start
+    x1, y1, z1 = end
+
+
+    # Incorporate the fraction of this segment covered by a dot into total reduction
+    length = math.sqrt( (x1-x0)**2 + (y1-y0)**2 + (z1 - z0)**2)
+    dotSizeConversion = .0565/20 # length units per dot size
+    convertedDotDiameter = dotSize * dotSizeConversion
+    lengthFracReduction = convertedDotDiameter / length
+    lengthFrac = lengthFrac - lengthFracReduction
+
+    # If the line segment should not cover the entire distance, get actual start and end coords
+    skipX = (x1-x0)*(1-lengthFrac)
+    skipY = (y1-y0)*(1-lengthFrac)
+    skipZ = (z1-z0)*(1-lengthFrac)
+    x0 = x0 + skipX/2
+    x1 = x1 - skipX/2
+    y0 = y0 + skipY/2
+    y1 = y1 - skipY/2
+    z0 = z0 + skipZ/2
+    z1 = z1 + skipZ/2
+
+    # Append line corresponding to the edge
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None) # Prevents a line being drawn from end of this edge to start of next edge
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+    edge_z.append(z0)
+    edge_z.append(z1)
+    edge_z.append(None)
+
+    # Draw arrow
+    if not arrowPos == None:
+
+        # Find the point of the arrow; assume is at end unless told middle
+        pointx = x1
+        pointy = y1
+        pointz = z1
+
+        eta = math.degrees(math.atan((x1-x0)/(y1-y0))) if y1!=y0 else 90.0
+
+        if arrowPos == 'middle' or arrowPos == 'mid':
+            pointx = x0 + (x1-x0)/2
+            pointy = y0 + (y1-y0)/2
+            pointz = z0 + (z1 - z0)/2
+
+
+        # Find the directions the arrows are pointing
+        signx = (x1-x0)/abs(x1-x0) if x1!=x0 else +1    #verify this once
+        signy = (y1-y0)/abs(y1-y0) if y1!=y0 else +1    #verified
+        signz = (z1-z0)/abs(z1-z0) if z1!=z0 else +1
+
+        # Append first arrowhead
+        dx = arrowLength * math.sin(math.radians(eta + arrowAngle))
+        dy = arrowLength * math.cos(math.radians(eta + arrowAngle))
+        dz = arrowLength * math.cos(math.radians(eta + arrowAngle))
+        edge_x.append(pointx)
+        edge_x.append(pointx - signx**2 * signy * dx)
+        edge_x.append(None)
+        edge_y.append(pointy)
+        edge_y.append(pointy - signx**2 * signy * dy)
+        edge_y.append(None)
+        edge_z.append(pointz)
+        edge_z.append(pointz - signx**1 * signy * dz)
+
+        # And second arrowhead
+        dx = arrowLength * math.sin(math.radians(eta - arrowAngle))
+        dy = arrowLength * math.cos(math.radians(eta - arrowAngle))
+        dz = arrowLength * math.cos(math.radians(eta - arrowAngle))
+        edge_x.append(pointx)
+        edge_x.append(pointx - signx**2 * signy * dx)
+        edge_x.append(None)
+        edge_y.append(pointy)
+        edge_y.append(pointy - signx**2 * signy * dy)
+        edge_y.append(None)
+        edge_y.append(pointz)
+        edge_y.append(pointz - signx**2 * signy * dz)
+        edge_y.append(None)
+
+
+    return edge_x, edge_y, edge_z
+
+def add_arrows(source_x: List[float], target_x: List[float], source_y: List[float], target_y: List[float],
+               arrowLength=0.025, arrowAngle=30):
+    pointx = list(map(lambda x: x[0] + (x[1] - x[0]) / 2, zip(source_x, target_x)))
+    pointy = list(map(lambda x: x[0] + (x[1] - x[0]) / 2, zip(source_y, target_y)))
+    etas = list(map(lambda x: math.degrees(math.atan((x[1] - x[0]) / (x[3] - x[2]))),
+                    zip(source_x, target_x, source_y, target_y)))
+
+    signx = list(map(lambda x: (x[1] - x[0]) / abs(x[1] - x[0]), zip(source_x, target_x)))
+    signy = list(map(lambda x: (x[1] - x[0]) / abs(x[1] - x[0]), zip(source_y, target_y)))
+
+    dx = list(map(lambda x: arrowLength * math.sin(math.radians(x + arrowAngle)), etas))
+    dy = list(map(lambda x: arrowLength * math.cos(math.radians(x + arrowAngle)), etas))
+    none_spacer = [None for _ in range(len(pointx))]
+    arrow_line_x = list(map(lambda x: x[0] - x[1] ** 2 * x[2] * x[3], zip(pointx, signx, signy, dx)))
+    arrow_line_y = list(map(lambda x: x[0] - x[1] ** 2 * x[2] * x[3], zip(pointy, signx, signy, dy)))
+
+    arrow_line_1x_coords = list(chain(*zip(pointx, arrow_line_x, none_spacer)))
+    arrow_line_1y_coords = list(chain(*zip(pointy, arrow_line_y, none_spacer)))
+
+    dx = list(map(lambda x: arrowLength * math.sin(math.radians(x - arrowAngle)), etas))
+    dy = list(map(lambda x: arrowLength * math.cos(math.radians(x - arrowAngle)), etas))
+    none_spacer = [None for _ in range(len(pointx))]
+    arrow_line_x = list(map(lambda x: x[0] - x[1] ** 2 * x[2] * x[3], zip(pointx, signx, signy, dx)))
+    arrow_line_y = list(map(lambda x: x[0] - x[1] ** 2 * x[2] * x[3], zip(pointy, signx, signy, dy)))
+
+    arrow_line_2x_coords = list(chain(*zip(pointx, arrow_line_x, none_spacer)))
+    arrow_line_2y_coords = list(chain(*zip(pointy, arrow_line_y, none_spacer)))
+
+    x_arrows = arrow_line_1x_coords + arrow_line_2x_coords
+    y_arrows = arrow_line_1y_coords + arrow_line_2y_coords
+
+    return x_arrows, y_arrows
+
