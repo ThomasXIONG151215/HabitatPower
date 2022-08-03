0e003e64ce1e9bb44c12667e83f46a23e48fff60
Thomas Yuanke <60814001+ThomasXIONG151215@users.noreply.github.com>
2022-08-03 20:27:49 +0800

    Update Intro_App.py

--- streamlit_apps/Intro_App.py
+++ streamlit_apps/Intro_App.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
   st.markdown('# Demo Introduction')
-  st.markdown('This demo app introduce the HabitatPower philosophy on the way energy system simulation is ought to be')
+  st.markdown('This demo app introduce the HabitatPower philosophy on the way energy system simulation is ought to be.')
   st.markdown('## Scenario Case ')
   st.markdown('The first scenario case of the app ever presented shall be a simple cold water refrigeration system. Engineers have to the best way to respond to a certain cooling demand by controlling various refrigeration parameters. ')
-  st.markdown('The system is composed by two chillers, four water pumps, two cooling towers and two air handling units: The water coming back from the air handling units could be regarded as a feedback signal from the environment alongside the thermal demand from the hall and the platform')
+  st.markdown('The system is composed by two chillers, four water pumps, two cooling towers and two air handling units: The water coming back from the air handling units could be regarded as a feedback signal from the environment alongside the thermal demand from the hall and the platform.')
