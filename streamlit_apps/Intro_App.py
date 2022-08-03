ed0fa0df18f7a950eced40e381bea8ee64d949c7
Thomas Yuanke <60814001+ThomasXIONG151215@users.noreply.github.com>
2022-08-03 20:41:11 +0800

    Update Intro_App.py

--- streamlit_apps/Intro_App.py
+++ streamlit_apps/Intro_App.py
@@ -7,4 +7,4 @@ def app():
   st.markdown('The first scenario case of the app ever presented shall be a simple cold water refrigeration system. Engineers have to the best way to respond to a certain cooling demand by controlling various refrigeration parameters. ')
   st.markdown('The system is mainly composed by two chillers, four water pumps, two cooling towers and two air handling units: The water coming back from the air handling units could be regarded as a feedback signal from the environment alongside the thermal demand from the hall and the platform.')
   st.image('./scenario_case_1.png')
-  st.write('Further controls onto the air-side managements will be developed alongside many other functionalities')
+  st.write('Further controls onto the air-side managements will be developed alongside many other functionalities.')
